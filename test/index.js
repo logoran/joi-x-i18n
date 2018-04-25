5df70f5adb1b2b61fe2629938aa89c08575dc81d
Leo Long <yujunlong2000@gmail.com>
2018-04-25 20:19:07 +0800

    update test use case

--- test/index.js
+++ test/index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const Lab = require('lab');
-const { describe, it } = exports.lab = Lab.script();
-// const lab = exports.lab = Lab.script();
-const expect = require('code').expect;
+const { describe, it, expect } = exports.lab = Lab.script();
 const joi = require('joi');
 
 describe('Initialize joi-x-i18n', () => {
@@ -13,6 +11,33 @@ describe('Initialize joi-x-i18n', () => {
 
   it('return a Joi instance if an instance of Joi is sended', () => {
     expect(require('../index')(joi)).to.include({isJoi: true});
+  });
+});
+
+describe('DefaultLocale joi-x-i18n', () => {
+  let Joi = require('../index')(joi);
+
+  it('return default locale seted', () => {
+    Joi.setDefaultLocale('zh_CN');
+    expect(Joi.getDefaultLocale()).to.equal('zh_CN');
+  });
+
+  it('return default locale seted null', () => {
+    Joi.setDefaultLocale(null);
+    expect(Joi.getDefaultLocale()).to.equal(undefined);
+  });
+
+  it('return default locale seted unknown', () => {
+    Joi.setDefaultLocale('unknown');
+    expect(Joi.getDefaultLocale()).to.equal(undefined);
+  });
+});
+
+describe('loadDirectory joi-x-i18n', () => {
+  let Joi = require('../index')(joi);
+
+  it('loadDirectory return the same instance sended', () => {
+    expect(Joi.loadDirectory(__dirname + '/tempdir', ['js'])).to.shallow.equal(Joi);
   });
 });
 
@@ -65,6 +90,22 @@ describe('Validate Schemas', () => {
 
   it('when a i18n option is sended and a correct directory have the translation `file`', () => {
     Joi = require('../index')(joi, __dirname + '/../locales');
+    Joi.validate({}, schema, {i18n: 'zh_CN'}, (err, data) => {
+      expect(err).to.include({isJoi: true, name: 'ValidationError'});
+      expect(err.details).to.part.include([{message: '"name" 是必须的'}]);
+    });
+  });
+
+  it('when a i18n option is sended and a correct directory have the translation `file`', () => {
+    Joi = require('../index')(Joi);
+    Joi.validate({}, schema, {i18n: 'en_US'}, (err, data) => {
+      expect(err).to.include({isJoi: true, name: 'ValidationError'});
+      expect(err.details).to.part.include([{message: '"name" is required'}]);
+    });
+  });
+
+  it('when a i18n option is sended and a correct directory have the translation `file`', () => {
+    Joi = require('../index')(Joi, {directory: __dirname + '/../locales', defaultLocale: 'zh_CN', extensions: ['json']});
     Joi.validate({}, schema, {i18n: 'zh_CN'}, (err, data) => {
       expect(err).to.include({isJoi: true, name: 'ValidationError'});
       expect(err.details).to.part.include([{message: '"name" 是必须的'}]);
